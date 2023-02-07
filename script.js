904136fb28fab50260d934960c02c9fb0bde4b90
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2023-02-07 21:05:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//Задание 1.
+
+const raiseToCube = (param1) => param1 ** 3;
+console.log(`Сумма равна ${raiseToCube(2) + raiseToCube(3)}.`);
+
+//Задание 2.
+
+// const salary = (money) => money * 0.87;
+    
+// const userMoney = parseInt(prompt('Сколько Вы зарабатываете?'));
+// if (isNaN(userMoney) || userMoney < 0) {
+//     alert('Ошибка! Введите не отрицательное число.');
+// }
+// let moneyMonth = salary(userMoney);
+// console.log(`Ваша зарплата за вычетом налога равна ${salary (userMoney)} RUR.`);
+
+//Задание 3.
+
+// function findMax (num1, num2, num3) {
+//     if (num1 < num2 && num2 > num3) {
+//         console.log(`Максимальное число ${num2}.`);
+//     } 
+//     if (num2 < num1 && num1 > num3){
+//         console.log(`Максимальное число ${num1}.`);
+//     } 
+//     if (num2 < num3 && num3 > num1){
+//         console.log(`Максимальное число ${num3}.`);
+//     }    
+// }
+
+// findMax (parseInt (prompt('Введите первое число: ')), parseInt (prompt('Введите второе число: ')), parseInt (prompt('Введите третье число: ')));
+
+//Задание 4.
+
+// const sum = (params1, params2) => params1 + params2;
+
+// const diff = (params1, params2) => {
+//     if (params1 > params2) {
+//         result = params1 - params2;
+//     } else {
+//         result = params2 - params1; 
+//     }
+//     return result;
+// }
+
+// const mult = (params1, params2) => params1 * params2;
+
+// const div = (params1, params2) => params1 / params2;
+
+// console.log(`Сумма: ${sum(11, 11)}, Разность: ${diff(11, 11)}, Произведение: ${mult(11, 11)}, Частное: ${div(11, 11)}.`);
+
+    
